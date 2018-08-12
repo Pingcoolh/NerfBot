640042054e09e2960be953c31c1f20137a56cc70
Kala30 <caleb.yun@gmail.com>
2018-08-12 09:34:17 -0700

    Tested add_field()

--- command_test.py
+++ command_test.py
@@ -8,17 +8,25 @@ file = open("token.txt", "r")
 TOKEN = file.read()
 file.close()
 
-''' EXAMPLE
 client = commands.Bot(command_prefix = '!')
 
 
-@client.event()
+@client.event
 async def on_ready():
     print('Bot is ready.')
 
 @client.command()
 async def ping():
     await client.say('Pong!')
+
+@client.command()
+async def test():
+    embed = discord.Embed(title="Test")
+    embed.add_field(name="Name", value="Value")
+    embed.add_field(name="Name", value="Value")
+    embed.add_field(name="Name", value="Value")
+    embed.add_field(name="Name", value="Value")
+    await client.say(embed=embed)
 
 @client.command()
 async def echo(*args):
@@ -29,4 +37,3 @@ async def echo(*args):
     await client.say(output)
 
 client.run(TOKEN)
-'''
